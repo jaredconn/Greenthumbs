3c492b831e56d5ea137e3b37d5bdbf7235011094
Lam Ngo <LamNgo725@gmail.com>
2018-04-02 16:43:31 -0700

    Room Database stuff

---
+++ crop/app/src/main/java/com/example/j/crop/AppDatabase.java
@@ -0,0 +1,28 @@
+package com.example.j.crop;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+/**
+ * Created by lhn41 on 4/2/2018.
+ */
+@Database(entities = {Plant.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase{
+    private static Builder<AppDatabase> INSTANCE;
+    public abstract DatabaseFunctions databaseFunc();
+
+    public static Builder<AppDatabase> getAppDatabase(Context context){
+        if(INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class, "plant-database");
+
+        }
+        return INSTANCE;
+    }
+
+    public static void destoryInstance(){
+        INSTANCE = null;
+    }
+
+}
